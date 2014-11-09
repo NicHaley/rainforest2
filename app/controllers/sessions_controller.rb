b8372e271291432a9258d13909d4e7005dc8e63f
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-11-09 09:38:48 -0500

    Added login functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+	def new
+	end
+
+	# Note: We don't have to instantiate a user object in our new action because 
+	# we are only logging in, not creating a new user.
+
+	def create
+  	user = User.find_by(email: params[:email])	# Find user matching input email in db
+  	if user && user.authenticate(params[:password])  # If user exists and password correct
+  		session[:user_id] = user.id				# Create a key/value pair
+  		redirect_to products_url, notice: "Logged in!"
+  	else
+  		render "new"
+  	end
+  end
+
+  def destroy
+  	session[:user_id] = nil		# Set user sign in to nil (ie. no one is signed in)
+  	redirect_to products_url, notice: "Logged out!"
+  end
+end
