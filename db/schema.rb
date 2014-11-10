13437d1339c053033a49e9dcf6ffa99ca036653b
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-11-09 20:00:23 -0500

    Added product images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141109170518) do
+ActiveRecord::Schema.define(version: 20141110002506) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141109170518) do
     t.integer  "price_in_cents"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
   create_table "reviews", force: true do |t|
@@ -34,7 +35,7 @@ ActiveRecord::Schema.define(version: 20141109170518) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"       # name added using rails g migration
+    t.string   "name"
   end
 
 end
