876d87133a7a92198f830ed022a866bd40cadbfb
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-11-10 16:18:31 -0500

    Wrote product tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe Product do
+	let!(:user) { create(:user) }
+
+	# Question 1
+
+	it "can be created with a password and confirmation"
+		
+	end
+
+end
